5baa1ff9d4e38123486c574ed3f499c3feb6e800
vladavanyan <vladimir.avanyan@student.rau.am>
2022-05-20 01:42:47 +0400

    oops

--- RequestRepository/RequestRepository.cpp
+++ RequestRepository/RequestRepository.cpp
@@ -2,8 +2,7 @@
 #include "RequestRepository.h"
 
     RequestRepository::RequestRepository() = default ;
-    RequestRepository::RequestRepository(std::string method, std::string url, std::string path,std::string version, std::string body): 
-                      method{method},url{url}, path{path}, version{version}, body{body}
+    RequestRepository::RequestRepository(std::string method, std::string url, std::string path,std::string version, std::string body): method{method},url{url}, path{path}, version{version}, body{body}
     {}
    
 RequestRepository RequestRepository::getRequest(int clientSocket) {
